d79b9c618a153cfd870292dd2c4c6d8a13871734
Léo Peltier <contact@leo-peltier.fr>
2016-06-22 15:49:53 +0200

    Shellcheck pass
    Everything might be broken now.

--- bashback.sh
+++ bashback.sh
@@ -11,58 +11,54 @@ txttab='\e[60G'   # Tab
 
 # Logs something to stdout
 function bb::log() {
-	echo -e "$txtblu[BB $(date +%s)]$txtrst $@"
+	echo -e "$txtblu[BB $(date +%s)]$txtrst $*"
 }
-
 
 # Logs something to stderr
 function bb::error() {
-	echo -e "$txtred[BB $(date +%s)]$txtrst $@" 1>&2
+	echo -e "$txtred[BB $(date +%s)]$txtrst $*" 1>&2
 }
-
 
 # Outputs the current date in a proper format
 function bb::date() {
 	date --rfc-3339=s | sed 's/ /_/g'
 }
 
-
 # Uses tar on all folders contained in a remote folder.
 # bb::tar_folders [login@]host dir_remote dir_local
 function bb::tar_folders() {
-	local ssh="ssh $1"
-	local dir_remote="$2"
-	local dir_local="$3"
-	local date="$(bb::date)"
+	local -r ssh="ssh $1"
+	local -r dir_remote="$2"
+	local -r dir_local="$3"
+	local -r date="$(bb::date)"
 	local error=0
 
 	for folder in $($ssh "cd '$dir_remote' && find ./* -maxdepth 0 -type d"); do
-		local folder="$(basename "$folder")"
-		local archive="$dir_local/${folder}_$(bb::date).tar.bz2"
+		local -r folder="$(basename "$folder")"
+		local -r archive="$dir_local/${folder}_${date}.tar.bz2"
 
 		bb::log "Archiving $1:$dir_remote/$folder"
 		$ssh "cd '$dir_remote' && tar -cO '$folder'" | bzip2 > "$archive"
 
 		if [ ! $? -eq 0 ]; then
 			bb::error "Failed to archive $1:$dir_remote/$folder"
-			error=$(($error+1))
+			error=$((error+1))
 		fi
 	done
 
 	return $error
 }
 
-
 # Rsync a remote folder to a local one.
 # bb::rsync [login@]host:[remote_path] local_path
 function bb::rsync() {
-	local remote="$1"
-	local local="$2"
-	local options='-aq'
+	local -r remote="$1"
+	local -r local="$2"
+	local -r options='-aq'
 	local error=0
 
 	bb::log "Rsync from $remote to $local"
-	rsync $options $remote $local
+	rsync $options "$remote" "$local"
 	error=$?
 
 	if [ ! $error -eq 0 ]; then
@@ -72,7 +68,6 @@ function bb::rsync() {
 	return $error
 }
 
-
 # Get the list of MySQL databases in a server.
 # mysql and {performance,information}_schema will be omitted
 # bb::mysql_fetch_databases host user pass
@@ -81,16 +76,15 @@ function bb::mysql_fetch_databases() {
 	local user="$2"
 	local pass="$3"
 
-	echo "show databases;" | mysql -h$host -u$user -p$pass -N | grep -Ev '^(mysql|performance_schema|information_schema)$'
+	echo "show databases;" | mysql -h"$host" -u"$user" -p"$pass" -N | grep -Ev '^(mysql|performance_schema|information_schema)$'
 }
-
 
 # Dumps the schema of a MySQL database.
 # bb::mysql_dump_schema host user pass local_dir
 function bb::mysql_dump_schema() {
-	local host="$1"
-	local user="$2"
-	local pass="$3"
+	local -r host="$1"
+	local -r user="$2"
+	local -r pass="$3"
 	local error=0
 	local options="$5"
 	cd "$4"
@@ -98,77 +92,73 @@ function bb::mysql_dump_schema() {
 	# Notice the -d
 	options="$options --single-transaction --flush-logs --dump-date --routines --events -d"
 
-	for db in $(bb::mysql_fetch_databases $host $user $pass); do
-		local local="$db.schema_$(bb::date).sql"
+	for db in $(bb::mysql_fetch_databases "$host" "$user" "$pass"); do
+		local -r local="$db.schema_$(bb::date).sql"
 		bb::log "Dumping schema of $host:$db"
-		mysqldump -h$host -u$user -p$pass $options $db > $local
+		mysqldump -h"$host" -u"$user" -p"$pass" $options "$db" > "$local"
 
 		if [ ! $? -eq 0 ]; then
 			bb::error "Failed to dump schema of $host:$db"
-			error=$(($error+1))
+			error=$((error+1))
 		else
 			# Compress after the dump to avoid locking tables during the compression.
-			bzip2 $local
+			bzip2 "$local"
 		fi
 	done
 
 	return $error
 }
 
-
 # Dumps the data of a MySQL database.
 # bb::mysql_dump_data host user pass local_dir
 function bb::mysql_dump_data() {
-	local host="$1"
-	local user="$2"
-	local pass="$3"
-	local error=0
+	local -r host="$1"
+	local -r user="$2"
+	local -r pass="$3"
+	local -r error=0
 	local options="$5"
 	cd "$4"
 
 	# Notice the -t and the -q, we don't want to fill the RAM
 	options="$options --single-transaction --flush-logs --dump-date --routines --events -q -t"
 
-	for db in $(bb::mysql_fetch_databases $host $user $pass); do
-		local local="$db.data_$(bb::date).sql"
+	for db in $(bb::mysql_fetch_databases "$host" "$user" "$pass"); do
+		local -r local="$db.data_$(bb::date).sql"
 		bb::log "Dumping data of $host:$db"
-		mysqldump -h$host -u$user -p$pass $options $db > $local
+		mysqldump -h"$host" -u"$user" -p"$pass" $options "$db" > "$local"
 
 		if [ ! $? -eq 0 ]; then
 			bb::error "Failed to dump data of $host:$db"
-			error=$(($error+1))
+			error=$((error+1))
 		else
-			bzip2 $local
+			bzip2 "$local"
 		fi
 	done
 
 	return $error
 }
 
-
 # Dumps a whole MySQL server.
 # bb::mysql_dump host user pass local_dir
 function bb::mysql_dump(){
-	local host="$1"
-	local user="$2"
-	local pass="$3"
-	local path="$4"
-	local options="$5"
+	local -r host="$1"
+	local -r user="$2"
+	local -r pass="$3"
+	local -r path="$4"
+	local -r options="$5"
 	local error=0
 
-	bb::mysql_dump_schema $host $user $pass $path $options
+	bb::mysql_dump_schema "$host" "$user" "$pass" "$path" $options
 	error=$?
-	bb::mysql_dump_data $host $user $pass $path $options
-	error=$(($error+$?))
+	bb::mysql_dump_data "$host" "$user" "$pass" "$path" $options
+	error=$((error+$?))
 	return $error
 }
 
-
 # Deletes all files in a directory older than n days.
 function bb::purge() {
-	local dir="$1"
-	local days="$2"
+	local -r dir="$1"
+	local -r days="$2"
 
-	find "$dir" -maxdepth 1 -type f -mtime +$days -delete
+	find "$dir" -maxdepth 1 -type f -mtime "+$days" -delete
 }
-
